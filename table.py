4cf79a8b898ae78aa6b6396801e6bdacaeaa0d0f
hollowmade <artemtretakov991@gmail.com>
2025-12-02 12:35:26 +0300

    Изменение рабочего файла программмы.
    В программе появилась возможность добавлять диаграмму по стоимости продукта

--- table.py
+++ table.py
@@ -1,11 +1,20 @@
 from prettytable import PrettyTable
+import matplotlib.pyplot as diagram
 
 receipt = PrettyTable()
 receipt.field_names = ['№','Название продукта', 'Цена','Количество', 'Стоимость',]
+
+products = []
+pay = []
 
 for i in range(3):
     product = input('Укажите название продукта: ')
     price = int(input('Введите цену продукта: '))
     quantity = int(input('Укажите количество продукта: '))
-    receipt.add_row([i+1, product, f'{price} руб', f'{quantity} шт', f'{price*quantity} руб.'123])
+    receipt.add_row([i+1, product, f'{price} руб.', f'{quantity} шт.', f'{price*quantity} руб.'])
+    products.append(product)
+    pay.append(price*quantity)
+
 print(receipt)
+diagram.bar(products,pay)
+diagram.show()
